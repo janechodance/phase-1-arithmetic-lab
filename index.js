168f3682adbc8d845f51d7a5f788af3cf22480b4
Jane <janechodance@gmail.com>
2021-09-20 13:38:03 -0400

    commit tree 9b5e8ab278068dc8709c4eadf31aee2fa63b3542

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let multiply
+let num1 = 31
+let num2 = 2
+multiply = num1 *num2
+let random
+random = Math.floor(Math.random() * 10) + 1
+let mod
+let num3 = 8
+let num4 = 2
+mod = num3 / num4
+let max
+max = Math.max(1,4,7,20)
